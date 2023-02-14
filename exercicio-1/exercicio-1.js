c79f91b1b5f7703f5bf9b42b108d7d5c88d95885
Davis Lamenha Sampaio <davislamenha@gmail.com>
2023-02-14 19:13:37 -0300

    calculadora finalizada

--- exercicio-1/exercicio-1.js
+++ exercicio-1/exercicio-1.js
@@ -22,3 +22,33 @@ function mostrarDados() {
   const idade = 2023 - parseFloat(nascimento.value);
   spanIdade.innerText = idade;
 }
+
+// CALCULADORA
+
+const calcForm = document.getElementById('calc-form');
+
+calcForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  mostrarCalculo();
+});
+
+function calcularOperacao(operador) {
+  const numero1Input = document.getElementById('calc-numero1');
+  const numero2Input = document.getElementById('calc-numero2');
+  const numero1 = parseFloat(numero1Input.value);
+  const numero2 = parseFloat(numero2Input.value);
+  const operacao = `${numero1} ${operador} ${numero2}`;
+  return eval(operacao);
+}
+
+function mostrarCalculo() {
+  const soma = document.querySelector('.soma');
+  const subtracao = document.querySelector('.subtracao');
+  const divisao = document.querySelector('.divisao');
+  const multiplicacao = document.querySelector('.multiplicacao');
+
+  soma.innerText = calcularOperacao('+');
+  subtracao.innerText = calcularOperacao('-');
+  divisao.innerText = calcularOperacao('/');
+  multiplicacao.innerText = calcularOperacao('*');
+}
