59155f388618b6b2d96e4725636c76880c9f8eff
Davis Lamenha Sampaio <davislamenha@gmail.com>
2023-02-15 16:09:24 -0300

    calculadora de dano finalizada

--- exercicio-2/exercicio-2.js
+++ exercicio-2/exercicio-2.js
@@ -1,3 +1,4 @@
+// TESTE DE VELOCIDADE
 const tvForm = document.getElementById('tv-form');
 
 tvForm.addEventListener('submit', (e) => {
@@ -25,4 +26,76 @@ function compararVeiculos() {
   } else if (velocidadeVeiculo1 < velocidadeVeiculo2) {
     return `${nomeVeiculo2} é mais rápido!`;
   } else return `${nomeVeiculo1} e ${nomeVeiculo2} tem a mesma velocidade!`;
+}
+
+// CALCULADORA DE DANOS
+const calcForm = document.getElementById('calc-form');
+
+calcForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  mostrarInfo();
+});
+
+function mostrarInfo() {
+  const nomePersonagem1 = document.getElementById('calc-nome1').value;
+  const nomePersonagem2 = document.getElementById('calc-nome2').value;
+  const ataquePersonagem1 = parseFloat(
+    document.getElementById('calc-ataque1').value,
+  );
+  const defesaPersonagem2 = parseFloat(
+    document.getElementById('calc-defesa2').value,
+  );
+  const escudoPersonagem2 = document.getElementById('calc-escudo2').checked;
+  const escudo = escudoPersonagem2 ? 'Possui escudo!' : 'Não possui escudo!';
+  const dano = calcularDano();
+  const vida = sofrerDano();
+  const resultado =
+    vida > 0
+      ? `${nomePersonagem2} sobreviveu ao ataque!`
+      : `${nomePersonagem2} não sobreviveu ao ataque!`;
+
+  const h3Personagem1 = document.querySelector('.nome1');
+  const spanAtaque = document.querySelector('.ataque1');
+  const spanDanoCausado = document.querySelector('.dano1');
+  const h3Personagem2 = document.querySelector('.nome2');
+  const spanVida = document.querySelector('.vida2');
+  const spanDefesa = document.querySelector('.defesa2');
+  const spanEscudo = document.querySelector('.escudo2');
+  const spanResultado = document.querySelector('.resultado');
+
+  h3Personagem1.innerHTML = nomePersonagem1;
+  spanAtaque.innerHTML = ataquePersonagem1;
+  spanDanoCausado.innerHTML = dano;
+
+  h3Personagem2.innerHTML = nomePersonagem2;
+  spanVida.innerHTML = vida;
+  spanDefesa.innerHTML = defesaPersonagem2;
+  spanEscudo.innerHTML = escudo;
+
+  spanResultado.innerHTML = resultado;
+}
+
+function sofrerDano() {
+  const vidaPersonagem2 = parseFloat(
+    document.getElementById('calc-vida2').value,
+  );
+  const dano = calcularDano();
+  const vida = vidaPersonagem2 < dano ? 0 : vidaPersonagem2 - dano;
+  return vida;
+}
+
+function calcularDano() {
+  const ataquePersonagem1 = parseFloat(
+    document.getElementById('calc-ataque1').value,
+  );
+  const defesaPersonagem2 = parseFloat(
+    document.getElementById('calc-defesa2').value,
+  );
+  const escudoPersonagem2 = document.getElementById('calc-escudo2').checked;
+
+  if (ataquePersonagem1 > defesaPersonagem2 && !escudoPersonagem2) {
+    return ataquePersonagem1 - defesaPersonagem2;
+  } else if (ataquePersonagem1 > defesaPersonagem2 && escudoPersonagem2) {
+    return (ataquePersonagem1 - defesaPersonagem2) / 2;
+  } else return 0;
 }
