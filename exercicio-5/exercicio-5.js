5e523bacd9fb69d7c2be0cc2ca8e812b9ac01639
Davis Lamenha Sampaio <davislamenha@gmail.com>
2023-02-20 11:04:16 -0300

    template string no prompt do menu

--- exercicio-5/exercicio-5.js
+++ exercicio-5/exercicio-5.js
@@ -9,14 +9,15 @@ miBtn.addEventListener('click', (e) => {
 function iniciarMenu() {
   let opcao = '';
   do {
-    opcao = prompt(
-      'Digite o numero da opção desejada:\n' +
-        '\n 1 - Opção 1' +
-        '\n 2 - Opção 2' +
-        '\n 3 - Opção 3' +
-        '\n 4 - Opção 4' +
-        '\n 5 - Encerrar',
-    );
+    opcao = prompt(`
+    Digite o numero da opção desejada: 
+
+    1 - Opção 1
+    2 - Opção 2
+    3 - Opção 3
+    4 - Opção 4
+    5 - Encerrar
+    `);
     if (opcao) alert(`Você escolheu a opção ${opcao}`);
   } while (opcao && opcao != '5');
   alert('O programa foi encerrado!');
