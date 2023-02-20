a2d8282d7504192c0cf96fc71c54b2678fc0b67a
Davis Lamenha Sampaio <davislamenha@gmail.com>
2023-02-20 16:26:55 -0300

    procurando palindromo finalizado

--- exercicio-6/exercicio-6.js
+++ exercicio-6/exercicio-6.js
@@ -1,3 +1,4 @@
+// ROBO DA TABUADA
 const rtForm = document.getElementById('rt-form');
 
 rtForm.addEventListener('submit', (e) => {
@@ -20,4 +21,39 @@ function mostrarTabuada() {
   const tabuada = calcularTabuada(numero);
 
   container.innerHTML = tabuada;
+}
+
+// PROCURANDO PALINDROMOS
+const ppForm = document.getElementById('pp-form');
+const palavra = document.getElementById('pp-palavra');
+
+ppForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  mostrarResultado();
+});
+
+function inverterPalavra(palavra) {
+  let invertida = '';
+  for (let i = palavra.length; i > 0; i--) {
+    invertida += palavra[i - 1];
+  }
+  return invertida;
+}
+
+function verificarPalindromo(palavra, palavraInvertida) {
+  return palavra.toLowerCase() === palavraInvertida.toLowerCase()
+    ? 'É um palíndromo!'
+    : 'Não é um palíndromo!';
+}
+
+function mostrarResultado() {
+  const p1 = document.querySelector('.palavra-1');
+  const p2 = document.querySelector('.palavra-2');
+  const resultado = document.querySelector('.resultado');
+
+  const palavraInvertida = inverterPalavra(palavra.value);
+
+  p1.innerHTML = palavra.value.toLowerCase();
+  p2.innerHTML = palavraInvertida.toLowerCase();
+  resultado.innerHTML = verificarPalindromo(palavra.value, palavraInvertida);
 }
